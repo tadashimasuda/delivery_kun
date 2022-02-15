8a6b35050819a13c04cf2efb95168c3eb3adaef0
tadashimasuda <tmasu4260@gmail.com>
2022-02-15 17:12:36 +0900

    main.dartからMapScreenの切り離し，componentsディレクトリ作成
    fixed #5

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './screens/login_screen.dart';
+import '../screens/login_screen.dart';
 
 class mainDrawer extends StatelessWidget {
   @override
---
+++ lib/screens/MapScreen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
+import 'dart:async';
+
+import 'package:delivery_kun/components/main_drawer.dart';
+import 'package:delivery_kun/components/slide_sheet_container.dart';
+
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  late LatLng _initialPosition;
+  late bool _loading;
+
+  void _getUserLocation() async {
+    final hasPermission = await _handlePermission();
+
+    if (!hasPermission) {
+      return;
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      _loading = false;
+    });
+  }
+
+  Future<bool> _handlePermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // do stuff
+      return false;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // do stuff
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      // do stuff
+
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+    _getUserLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawerEnableOpenDragGesture: false,
+      drawer: Drawer(
+        child: mainDrawer(),
+      ),
+      backgroundColor: Colors.grey.shade200,
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: Builder(
+        builder: (context) =>
+            FloatingActionButton(
+              elevation: 20,
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: Colors.blue, width: 3),
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"),
+                    )),
+              ),
+            ),
+      ),
+      body: SlidingSheet(
+        elevation: 8,
+        cornerRadius: 16,
+        snapSpec: const SnapSpec(
+          // Enable snapping. This is true by default.
+            snap: true,
+            // Set custom snapping points.
+            snappings: [0.5, 0.7, 1.0],
+            // Define to what the snappings relate to. In this case,
+            // the total available space that the sheet can expand to.
+            positioning: SnapPositioning.relativeToSheetHeight),
+        // The body widget will be displayed under the SlidingSheet
+        // and a parallax effect can be applied to it.
+        body: Center(
+          child: _loading
+              ? CircularProgressIndicator()
+              : Container(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: _initialPosition,
+                    zoom: 14.4746,
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                  // markers: _createMarker(),
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                  mapToolbarEnabled: false,
+                  buildingsEnabled: true,
+                  onTap: (LatLng latLang) {
+                    print('Clicked: $latLang');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        builder: (context, state) {
+          return slideSheet();
+        },
+      ),
+    );
+  }
+}
