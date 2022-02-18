c8854c67109c31dcf7b4d5809d41f525a5da1360
tadashimasuda <tmasu4260@gmail.com>
2022-02-18 20:38:09 +0900

    create file

---
+++ lib/screens/order_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderList extends StatefulWidget {
+  const OrderList({Key? key}) : super(key: key);
+
+  @override
+  _OrderListState createState() => _OrderListState();
+}
+
+class _OrderListState extends State<OrderList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
