bc0d755dcf902dd2ad899407e991e0b8f9ec54ff
tadashimasuda <tmasu4260@gmail.com>
2022-02-18 19:49:45 +0900

    収益欄 受注履歴ボタン作成

--- lib/screens/order_record.dart
+++ lib/screens/order_record.dart
@@ -30,7 +30,7 @@ class _OrderRecordState extends State<OrderRecord> {
               color: Colors.grey,
             ),
             Container(
-              margin: EdgeInsets.only(right:15,left:15),
+              margin: EdgeInsets.only(right: 15, left: 15),
               child: Column(
                 children: [
                   Container(
@@ -43,26 +43,125 @@ class _OrderRecordState extends State<OrderRecord> {
                         ),
                       )),
                   Table(
+                      border: TableBorder(
+                        bottom:
+                            BorderSide(width: 0.5, color: Color(0xFF000000)),
+                      ),
                       children: [
-                        TableRow(
-                            children: [
-                          Text('オンライン',style: TextStyle(
-                            color: Colors.grey
-                          ),),
-                          Text('受注数',style: TextStyle(
-                              color: Colors.grey
-                          ))
+                        TableRow(children: [
+                          Text(
+                            'オンライン',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          Text('受注数', style: TextStyle(color: Colors.grey))
                         ]),
                         TableRow(children: [
-                          Text('8時間24分',style: TextStyle(
-                            fontSize: 20,
-                          ),),
-                          Text('21',
+                          TableCell(
+                            child: SizedBox(
+                              height: 50,
+                              child: Text(
+                                '8時間24分',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                          TableCell(
+                            child: SizedBox(
+                              height: 50,
+                              child: Text(
+                                '21',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                      ]),
+                  Container(
+                      alignment: Alignment.topLeft,
+                      margin: EdgeInsets.only(top: 10),
+                      child: Text('明細',style: TextStyle(
+                        fontSize: 30,
+                      ),)
+                  ),
+                  Table(children: [
+                    TableRow(children: [
+                      Text(
+                        '売上',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text('21000',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ))
+                    ]),
+                    TableRow(children: [
+                      TableCell(
+                        child: SizedBox(
+                          height: 50,
+                          child: Text(
+                            '支出',
                             style: TextStyle(
                               fontSize: 20,
-                            ),)
+                            ),
+                          ),
+                        ),
+                      ),
+                      TableCell(
+                        child: SizedBox(
+                          height: 50,
+                          child: Text(
+                            '1000',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      TableCell(
+                        child: SizedBox(
+                          height: 50,
+                          child: Text(
+                            '利益',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TableCell(
+                        child: SizedBox(
+                          height: 50,
+                          child: Text(
+                            '20000',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
                     ]),
                   ]),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: Text('受注履歴を見る',),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      minimumSize: Size(double.infinity, 55),
+                    ),
+                  ),
                 ],
               ),
             )
