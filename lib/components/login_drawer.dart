483dcec6715879bb9a0167f6082fb03664d030f9
tadashimasuda <tmasu4260@gmail.com>
2022-03-29 17:36:48 +0900

    ユーザ画像の変更

--- lib/components/login_drawer.dart
+++ lib/components/login_drawer.dart
@@ -24,8 +24,7 @@ class _LoginDrawerState extends State<LoginDrawer> {
               accountEmail: Text(''),
               currentAccountPicture: CircleAvatar(
                 backgroundColor: Colors.white,
-                backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"),
+                backgroundImage: AssetImage("images/user.png")
               ),
             ),
           ]),
