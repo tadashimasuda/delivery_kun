3c99b839588488b7a8fe2a3ff3e35a62f0c34a10
tadashimasuda <tmasu4260@gmail.com>
2022-03-30 23:00:38 +0900

    baseURLの変更

--- lib/services/dio.dart
+++ lib/services/dio.dart
@@ -3,7 +3,8 @@ import 'package:dio/dio.dart';
 Dio dio(){
   var dio = new Dio();
 
-  dio.options.baseUrl = "http://localhost:8000/api";
+  // dio.options.baseUrl = "http://localhost:8000/api";
+  dio.options.baseUrl = "https://deliverykun-server.xyz/api";
   dio.options.headers['Accept'] = 'Application/json';
 
   return dio;
