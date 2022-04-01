6cc629e1b0ac08602b9c9614b1de8998bdd28ce7
tadashimasuda <tmasu4260@gmail.com>
2022-04-01 20:22:54 +0900

    パスワード欄タップでもvalueを維持

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -61,157 +61,159 @@ class _SettingScreenState extends State<SettingScreen> with ValidateText {
       appBar: AppBar(
         title: Text('ユーザー設定'),
       ),
-      body: Container(
-          padding: EdgeInsets.fromLTRB(24, 70, 24, 0),
-          child: Consumer<Auth>(builder: (context, auth, _) {
-            return auth.user != null ? Column(
-              children: [
-                AccountTextField(
-                  inputText: auth.user.name,
-                  obscureText: false,
-                  title: 'ユーザ名',
-                  icon: Icons.person,
-                  isBorder: true,
-                  onChange: (value) {
-                    _name = value;
-                  },
-                ),
-                Column(
-                  children: ValidateUserName(auth.validate_message),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                AccountTextField(
-                  inputText: auth.user.email,
-                  obscureText: false,
-                  title: 'メールアドレス',
-                  icon: Icons.mail,
-                  isBorder: true,
-                  onChange: (value) {
-                    _email = value;
-                  },
-                ),
-                Column(
-                  children: ValidateEmail(auth.validate_message),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  controller: TextEditingController(
-                      text: _VehicleModelList[_selectVehicleModelId]),
-                  readOnly: true,
-                  decoration: InputDecoration(
-                      labelText: '配達車両',
-                      suffixIcon: vehiceModelIcon(_selectVehicleModelId),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)))),
-                  onTap: () {
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Container(
-                            height: MediaQuery.of(context).size.height / 3,
-                            child: CupertinoPicker(
-                              itemExtent: 30,
-                              onSelectedItemChanged: (val) {
-                                setState(() {
-                                  _selectVehicleModelId = val;
-                                });
-                              },
-                              children: _VehicleModelList.map((e) => Text(e))
-                                  .toList(),
-                              scrollController: FixedExtentScrollController(
-                                  initialItem: auth.user.vehicleModel),
-                            ),
-                          );
-                        });
-                  },
-                ),
-                Column(
-                  children: ValidateVehicleModel(auth.validate_message),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  controller: TextEditingController(
-                      text: _prefectureList[_selectPrefectureId]),
-                  readOnly: true,
-                  decoration: InputDecoration(
-                      hintText: _prefectureList[_selectPrefectureId],
-                      suffixIcon: Icon(Icons.location_on),
-                      labelText: '活動場所',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)))),
-                  onTap: () {
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Container(
-                            height: MediaQuery.of(context).size.height / 3,
-                            child: CupertinoPicker(
-                              itemExtent: 30,
-                              onSelectedItemChanged: (val) {
-                                setState(() {
-                                  _selectPrefectureId = val;
-                                });
-                              },
-                              children:
-                                  _prefectureList.map((e) => Text(e)).toList(),
-                              scrollController: FixedExtentScrollController(
-                                  initialItem: _selectPrefectureId),
-                            ),
-                          );
-                        });
-                  },
-                ),
-                Column(
-                  children: ValidatePrefecture(auth.validate_message),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                SubmitBtn(
-                    title: '更新',
-                    color: Colors.lightBlue,
-                    onTap: () async {
-                      Map userData = {
-                        'name': _name,
-                        'email': _email,
-                        'vehicleModelId': _selectVehicleModelId,
-                        'prefectureId': _selectPrefectureId+1,
-                      };
-                      bool response =
-                          await Provider.of<Auth>(context, listen: false)
-                              .updateUser(userData: userData);
+      body: SingleChildScrollView(
+        child: Container(
+            padding: EdgeInsets.fromLTRB(24, 70, 24, 0),
+            child: Consumer<Auth>(builder: (context, auth, _) {
+              return auth.user != null ? Column(
+                children: [
+                  AccountTextField(
+                    controller:TextEditingController(text: auth.user.name),
+                    obscureText: false,
+                    title: 'ユーザ名',
+                    icon: Icons.person,
+                    isBorder: true,
+                    onChange: (value) {
+                      _name = value;
+                    },
+                  ),
+                  Column(
+                    children: ValidateUserName(auth.validate_message),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  AccountTextField(
+                    controller:TextEditingController(text: auth.user.email),
+                    obscureText: false,
+                    title: 'メールアドレス',
+                    icon: Icons.mail,
+                    isBorder: true,
+                    onChange: (value) {
+                      _email = value;
+                    },
+                  ),
+                  Column(
+                    children: ValidateEmail(auth.validate_message),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: TextEditingController(
+                        text: _VehicleModelList[_selectVehicleModelId]),
+                    readOnly: true,
+                    decoration: InputDecoration(
+                        labelText: '配達車両',
+                        suffixIcon: vehiceModelIcon(_selectVehicleModelId),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)))),
+                    onTap: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              height: MediaQuery.of(context).size.height / 3,
+                              child: CupertinoPicker(
+                                itemExtent: 30,
+                                onSelectedItemChanged: (val) {
+                                  setState(() {
+                                    _selectVehicleModelId = val;
+                                  });
+                                },
+                                children: _VehicleModelList.map((e) => Text(e))
+                                    .toList(),
+                                scrollController: FixedExtentScrollController(
+                                    initialItem: auth.user.vehicleModel),
+                              ),
+                            );
+                          });
+                    },
+                  ),
+                  Column(
+                    children: ValidateVehicleModel(auth.validate_message),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: TextEditingController(
+                        text: _prefectureList[_selectPrefectureId]),
+                    readOnly: true,
+                    decoration: InputDecoration(
+                        hintText: _prefectureList[_selectPrefectureId],
+                        suffixIcon: Icon(Icons.location_on),
+                        labelText: '活動場所',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)))),
+                    onTap: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              height: MediaQuery.of(context).size.height / 3,
+                              child: CupertinoPicker(
+                                itemExtent: 30,
+                                onSelectedItemChanged: (val) {
+                                  setState(() {
+                                    _selectPrefectureId = val;
+                                  });
+                                },
+                                children:
+                                    _prefectureList.map((e) => Text(e)).toList(),
+                                scrollController: FixedExtentScrollController(
+                                    initialItem: _selectPrefectureId),
+                              ),
+                            );
+                          });
+                    },
+                  ),
+                  Column(
+                    children: ValidatePrefecture(auth.validate_message),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SubmitBtn(
+                      title: '更新',
+                      color: Colors.lightBlue,
+                      onTap: () async {
+                        Map userData = {
+                          'name': _name,
+                          'email': _email,
+                          'vehicleModelId': _selectVehicleModelId,
+                          'prefectureId': _selectPrefectureId+1,
+                        };
+                        bool response =
+                            await Provider.of<Auth>(context, listen: false)
+                                .updateUser(userData: userData);
 
-                      if (response) {
-                        showCupertinoDialog(
-                            context: context,
-                            builder: (context) {
-                              return CupertinoAlertDialog(
-                                title: Text('更新されました！',style: TextStyle(color: Colors.black),),
-                                actions: [
-                                  CupertinoDialogAction(
-                                    isDestructiveAction: true,
-                                    child: Text('OK',style: TextStyle(color: Colors.blueAccent)),
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => MapScreen()));
-                                    },
-                                  ),
-                                ],
-                              );
-                            });
-                      }
-                    })
-              ],
-            ):Center(child: CircularProgressIndicator());
-          })),
+                        if (response) {
+                          showCupertinoDialog(
+                              context: context,
+                              builder: (context) {
+                                return CupertinoAlertDialog(
+                                  title: Text('更新されました！',style: TextStyle(color: Colors.black),),
+                                  actions: [
+                                    CupertinoDialogAction(
+                                      isDestructiveAction: true,
+                                      child: Text('OK',style: TextStyle(color: Colors.blueAccent)),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => MapScreen()));
+                                      },
+                                    ),
+                                  ],
+                                );
+                              });
+                        }
+                      })
+                ],
+              ):Center(child: CircularProgressIndicator());
+            })),
+      ),
       bottomNavigationBar:  Container(
         height: _bannerAd.size.height.toDouble(),
         width: _bannerAd.size.width.toDouble(),
