4b7433986b8bc6f7db4edbfca023467e298aeadb
tadashimasuda <tmasu4260@gmail.com>
2022-04-21 23:54:05 +0900

    apple signIn モーダル表示

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_kun/models/validate.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:delivery_kun/screens/map_screen.dart';
 import 'package:delivery_kun/screens/sign_up_screen.dart';
 import 'package:delivery_kun/services/auth.dart';
@@ -30,7 +30,7 @@ class _SignInFormState extends State<SignInForm> with ValidateText {
     ],
   );
 
-  Future<bool> _handleSignIn() async {
+  Future<bool> _handleSignInGoogle() async {
     try {
       var response = await _googleSignIn.signIn();
       if (response != null) {
@@ -56,186 +56,294 @@ class _SignInFormState extends State<SignInForm> with ValidateText {
       return false;
     }
   }
-  
+
+  Future<bool> _handleSignInApple() async {
+    try {
+      final credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+      print(credential);
+      return false;
+
+    } catch (error) {
+    print(error);
+    return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
             child: Padding(
-          padding: EdgeInsets.fromLTRB(35, 40, 35, 0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'おかえりなさい！',
-                style: TextStyle(
-                    color: Colors.lightBlue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'デリバリーくんにログイン',
-                style: TextStyle(
-                    fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Consumer<Auth>(
-                  builder: (context, auth, child) => Form(
-                    child:Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          AccountTextField(
-                            obscureText: false,
-                            title: 'メールアドレス',
-                            icon: Icons.mail,
-                            onChange: (value) {
-                              email = value;
-                            },
-                          ),
-                          Column(
-                            children: ValidateEmail(auth.validate_message),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          AccountTextField(
-                            obscureText: true,
-                            title: 'パスワード',
-                            icon: Icons.password,
-                            onChange: (value) {
-                              password = value;
-                            },
-                          ),
-                          Column(
-                            children: ValidatePassword(auth.validate_message),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                        ]),
-                  )
-              ),
-              SubmitBtn(
-                title: 'ログイン',
-                color: Colors.lightBlue,
-                onTap: () async {
-                  Map creds = {
-                    'email': email,
-                    'password': password,
-                  };
-                  bool response = await Provider.of<Auth>(context, listen: false)
-                      .login(creds: creds);
-                  if (response) {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MapScreen()));
-                  }
-                },
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text('または', textAlign: TextAlign.center),
-              SizedBox(
-                height: 15,
-              ),
-              GestureDetector(
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.grey)),
-                  width: MediaQuery.of(context).size.width,
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 25,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  'https://github.com/sbis04/flutterfire-samples/blob/google-sign-in/assets/google_logo.png?raw=true')),
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Text(
-                        'Googleでログイン',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
+              padding: EdgeInsets.fromLTRB(35, 40, 35, 0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 50,
                   ),
-                ),
-                onTap: () async {
-                  setState(() {
-                    isLoading = false;
-                  });
+                  Text(
+                    'おかえりなさい！',
+                    style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'デリバリーくんにログイン',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Consumer<Auth>(
+                      builder: (context, auth, child) =>
+                          Form(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  AccountTextField(
+                                    obscureText: false,
+                                    title: 'メールアドレス',
+                                    icon: Icons.mail,
+                                    onChange: (value) {
+                                      email = value;
+                                    },
+                                  ),
+                                  Column(
+                                    children: ValidateEmail(
+                                        auth.validate_message),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  AccountTextField(
+                                    obscureText: true,
+                                    title: 'パスワード',
+                                    icon: Icons.password,
+                                    onChange: (value) {
+                                      password = value;
+                                    },
+                                  ),
+                                  Column(
+                                    children: ValidatePassword(
+                                        auth.validate_message),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                ]),
+                          )
+                  ),
+                  SubmitBtn(
+                    title: 'ログイン',
+                    color: Colors.lightBlue,
+                    onTap: () async {
+                      Map creds = {
+                        'email': email,
+                        'password': password,
+                      };
+                      bool response = await Provider.of<Auth>(
+                          context, listen: false)
+                          .login(creds: creds);
+                      if (response) {
+                        Navigator.push(context,
+                            MaterialPageRoute(
+                                builder: (context) => MapScreen()));
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text('または', textAlign: TextAlign.center),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  GestureDetector(
+                    child: SocialSignInButton(
+                      backgroundColor: Colors.white,
+                      textColor: Colors.black,
+                      title: 'Googleでログイン',
+                      image: DecorationImage(image: NetworkImage(
+                          'https://github.com/sbis04/flutterfire-samples/blob/google-sign-in/assets/google_logo.png?raw=true'),),
+                    ),
+                    onTap: () async {
+                      setState(() {
+                        isLoading = false;
+                      });
 
-                  bool isLogin = await _handleSignIn();
+                      bool isLogin = await _handleSignInGoogle();
 
-                  if (isLogin) {
-                    setState(() {
-                      isLoading = true;
-                    });
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MapScreen()));
-                  } else {
-                    setState(() {
-                      isLoading = true;
-                    });
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return CupertinoAlertDialog(
-                          title: Text("ログインに失敗しました。"),
-                          actions: <Widget>[
-                            CupertinoDialogAction(
-                              child: Text("OK"),
-                              isDestructiveAction: true,
-                              onPressed: () => Navigator.pop(context),
-                            ),
-                          ],
+                      if (isLogin) {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MapScreen()));
+                      } else {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return CupertinoAlertDialog(
+                              title: Text("ログインに失敗しました。"),
+                              actions: <Widget>[
+                                CupertinoDialogAction(
+                                  child: Text("OK"),
+                                  isDestructiveAction: true,
+                                  onPressed: () => Navigator.pop(context),
+                                ),
+                              ],
+                            );
+                          },
                         );
-                      },
-                    );
-                  }
-                },
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  GestureDetector(
+                    child: SocialSignInButton(
+                      backgroundColor: Colors.black,
+                      textColor: Colors.white,
+                      title: 'Appleでログイン',
+                      image: DecorationImage(
+                          image: AssetImage("images/apple_login.png")),
+                    ),
+                    onTap: () async {
+                      setState(() {
+                        isLoading = false;
+                      });
+
+                      bool isLogin = await _handleSignInApple();
+
+                      if (isLogin) {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MapScreen()));
+                      } else {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return CupertinoAlertDialog(
+                              title: Text("ログインに失敗しました。"),
+                              actions: <Widget>[
+                                CupertinoDialogAction(
+                                  child: Text(
+                                    "OK",
+                                    style: TextStyle(
+                                      color: Colors.black
+                                    ),
+                                  ),
+                                  isDestructiveAction: true,
+                                  onPressed: () => Navigator.pop(context),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpForm()));
+                    },
+                    child: Row(
+                      children: [
+                        Text('アカウントをお持ちでない方は',
+                          style: TextStyle(color: Colors.grey),),
+                        Text(
+                          '登録',
+                          style: TextStyle(color: Colors.lightBlue),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SignUpForm()));
-                },
-                child: Row(
-                  children: [
-                    Text('アカウントをお持ちでない方は',style: TextStyle(color: Colors.grey),),
-                    Text(
-                    '登録',
-                    style: TextStyle(color: Colors.lightBlue),
-                  ),],
-                ),
-              )
-            ],
+            )),
+      ),
+    );
+  }
+}
+
+class SocialSignInButton extends StatelessWidget {
+  SocialSignInButton(
+      {required this.backgroundColor, required this.textColor, required this.title, required this.image});
+
+  final Color backgroundColor;
+  final Color textColor;
+  final String title;
+  final DecorationImage image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(0, 3),
+            )
+          ]
+      ),
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(width: 50,),
+          Container(
+            width: 25,
+            decoration: BoxDecoration(
+                image: image
+            ),
           ),
-        )),
+          SizedBox(width: 20,),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 18,
+            ),
+          ),
+        ],
       ),
     );
   }
